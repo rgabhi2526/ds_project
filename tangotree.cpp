c585deb588455039e031bb43bcccb68297325e8f
rgabhi2526 <143308195+rgabhi2526@users.noreply.github.com>
2024-04-27 21:13:01 +0530

    Updated

--- tangotree.cpp
+++ tangotree.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 class TangoTree 
 {
     struct Node 
@@ -11,14 +10,56 @@ class TangoTree
         Node* right;
         int height;
     };
+
+    int max(int a, int b) 
+    {
+        return (a > b) ? a : b; 
+    }
+
+    int getHeight(Node* node) 
+    {
+        if (node == NULL) 
+        {
+            return 0;
+        }
+        return node->height;
+    }
+
+    int getNum(Node* node) 
+    {
+        if (node == NULL) 
+        {
+            return 0;
+        }
+        return node->data;
+    }
+
+    Node *insert(Node *root, Node *child)
+    {
+        if(root == NULL)
+        {
+            return child;
+        }
+        if(child->data > root->data)
+        {
+            root->right = insert(root->right, child);
+        }
+        else
+        {
+            root->left = insert(root->left, child);
+        }
+        updateHeight(root);
+        return root;
+    }
+
 public:
     Node* root;
+
     TangoTree()
     {
-     root = NULL;
-    }
-    
-// creating newnode and inserting data into the new node
+        root = NULL;
+    } 
+
     Node* createNode(int data) 
     {
         Node* newNode = (Node*)malloc(sizeof(Node));
@@ -28,162 +69,185 @@ public:
         return newNode;
     }
 
-    int getHeight(Node* node) 
-    {
-        if (node == NULL) {
-            return 0;
-        }
-        return node->height;
-    }
-
     void updateHeight(Node* node) 
     {
-        if(getHeight(node->left) >= getHeight(node->left))
+        if(getHeight(node->left) >getHeight(node->right))
         {
          node->height = 1 + getHeight(node->left);
         }
-        else
+        else if(getHeight(node->left) <getHeight(node->right))
         {
          node->height = 1 + getHeight(node->right);  
         }
     }
 
-    Node* leftRotate(Node* z) {
-        Node* y = z->right;
-        Node* T2 = y->left;
-        y->left = z;
-        z->right = T2;
-        updateHeight(z);
-        updateHeight(y);
-        return y;
+    Node *rightRotate(Node *y)
+    {
+        Node *head = y->left;
+        Node *c_1 = head->right;
+        Node *c_2 = head->left;
+        y->left = NULL;
+        head->left = head->right = NULL;
+
+        if(getHeight(c_1) > getHeight(c_2))
+        {
+            head = insert(head, c_1);
+            if(c_2 == NULL)
+                head = insert(head, y);
+            else
+            {
+                if(getNum(c_2) > getNum(y))
+                {
+                    head = insert(head, c_2);
+                    head = insert(head, y);
+                }
+                else
+                {
+                    head = insert(head, y); 
+                    head = insert(head, c_2);
+                }
+            }
+        }
+        else if(getHeight(c_1) == getHeight(c_2))
+        {
+            head = insert(head, c_1);
+            if(getNum(c_2) > getNum(y))
+            {
+                head = insert(head, c_2);
+                head = insert(head, y);
+            }
+            else
+            {
+                head = insert(head, y); 
+                head = insert(head, c_2);
+            }
+        }
+        else
+        {
+            head = insert(head, c_2);
+            if(c_1 == NULL)
+                head = insert(head, y);
+            else
+            {
+                if(getNum(c_1) > getNum(y))
+                {
+                    head = insert(head, c_1);
+                    head = insert(head, y);
+                }
+                else
+                {
+                    head = insert(head, y); 
+                    head = insert(head, c_1);
+                }
+            }
+        }
+        return head;
     }
 
-    Node* rightRotate(Node* y) {
-        Node* x = y->left;
-        Node* T2 = x->right;
-        x->right = y;
-        y->left = T2;
-        updateHeight(y);
-        updateHeight(x);
-        return x;
+    Node *leftRotate(Node *x)
+    {
+        Node *head = x->right;
+        Node *c_1 = head->left;
+        Node *c_2 = head->right;
+        x->right = NULL;
+        head->left = head->right = NULL;
+
+        if(getHeight(c_1) > getHeight(c_2))
+        {
+            head = insert(head, c_1);
+            if(c_2 == NULL)
+                head = insert(head, x);
+            else
+            {
+                if(getNum(c_2) > getNum(x))
+                {
+                    head = insert(head, c_2);
+                    head = insert(head, x);
+                }
+                else
+                {
+                    head = insert(head, x); 
+                    head = insert(head, c_2);
+                }
+            }
+        }
+        else if(getHeight(c_1) == getHeight(c_2))
+        {
+            head = insert(head, c_1);
+            if(getNum(c_2) > getNum(x))
+            {
+                head = insert(head, c_2);
+                head = insert(head, x);
+            }
+            else
+            {
+                head = insert(head, x); 
+                head = insert(head, c_2);
+            }
+        }
+        else
+        {
+            head = insert(head, c_2);
+            if(c_1 == NULL)
+                head = insert(head, x);
+            else
+            {
+                if(getNum(c_1) > getNum(x))
+                {
+                    head = insert(head, c_1);
+                    head = insert(head, x);
+                }
+                else
+                {
+                    head = insert(head, x); 
+                    head = insert(head, c_1);
+                }
+            }
+        }
+        return head;
+    }
+
+    Node *balance(Node *head)
+    {
+        if(head == NULL)
+            return head;
+        else
+        {
+            balance(head->left);
+            balance(head->right);
+            int balanceFactor = getHeight(head->left) - getHeight(head->right);
+            if (balanceFactor > 1) 
+            {
+                return rightRotate(head);
+            } 
+            else if (balanceFactor < -1) 
+            {
+                return leftRotate(head);
+            }
+        }
+        return head;
     }
 
     Node* insertNode(Node* node, int data) 
     {
-        if (node == NULL) {
+        if (node == NULL) 
+        {
             return createNode(data);
         }
-        if (data < node->data) {
+        if (data < node->data) 
+        {
             node->left = insertNode(node->left, data);
-        } else if (data > node->data) {
+        } 
+        else if (data > node->data) 
+        {
             node->right = insertNode(node->right, data);
         } 
 
         updateHeight(node);
-        int balanceFactor = getHeight(node->left) - getHeight(node->right);
-        if (balanceFactor > 1) 
-        {
-            if (data < node->left->data) 
-            {
-                return rightRotate(node);
-            }
-            else 
-            {
-                node->left = leftRotate(node->left);
-                return rightRotate(node);
-            }
-        } 
-        else if (balanceFactor < -1) 
-        {
-            if (data > node->right->data) 
-            {
-                return leftRotate(node);
-            } 
-            else 
-            {
-                node->right = rightRotate(node->right);
-                return leftRotate(node);
-            }
-        }
+        node = balance(node);
         return node;
     }
-
-    Node* minValueNode(Node* node) {
-    Node* current = node;
-    while (current && current->left != NULL)
-        current = current->left;
-    return current;
-}
-
-Node* deleteNode(Node* root, int data) {
-    if (root == NULL)
-        return root;
-
-    if (data < root->data)
-        root->left = deleteNode(root->left, data);
-
-    else if(data > root->data)
-        root->right = deleteNode(root->right, data);
-
-    else {
-        if( (root->left == NULL) || (root->right == NULL) ) {
-            Node* temp = root->left ? root->left : root->right;
-
-            if(temp == NULL) {
-                temp = root;
-                root = NULL;
-            }
-            else
-                *root = *temp;
-
-            free(temp);
-        }
-        else {
-            Node* temp = minValueNode(root->right);
-            root->data = temp->data;
-            root->right = deleteNode(root->right, temp->data);
-        }
-    }
-
-    if (root == NULL)
-        return root;
-
-    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
-
-    int balanceFactor = getHeight(root->left) - getHeight(root->right);
-
-    if (balanceFactor > 1 && getHeight(root->left->left) >= getHeight(root->left->right))
-        return rightRotate(root);
-
-    if (balanceFactor > 1 && getHeight(root->left->right) > getHeight(root->left->left)) {
-        root->left =  leftRotate(root->left);
-        return rightRotate(root);
-    }
-
-    if (balanceFactor < -1 && getHeight(root->right->right) >= getHeight(root->right->left))
-        return leftRotate(root);
-
-    if (balanceFactor < -1 && getHeight(root->right->left) > getHeight(root->right->right)) {
-        root->right = rightRotate(root->right);
-        return leftRotate(root);
-    }
-
-    return root;
-}
 };
 
-int max(int a, int b) {
-    return (a > b)? a : b;
-}
-
-int main() {
-    TangoTree tree;
-    tree.root = tree.insertNode(tree.root, 10);
-    tree.root = tree.insertNode(tree.root, 20);
-    tree.root = tree.insertNode(tree.root, 5);
-    tree.root = tree.insertNode(tree.root, 15);
 
 
-    tree.root = tree.deleteNode(tree.root, 15);
-    return 0;
-}
